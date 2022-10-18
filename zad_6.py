4d9f8d406652ad492124f422e835ccb096c9276a
Przemek <email@email.com>
2022-10-18 14:25:16 +0200

    zapomnialem spotegowac

--- zad_6.py
+++ zad_6.py
@@ -1,5 +1,5 @@
 def zad_6(arr1: list, arr2: list) -> list:
-    return list(dict.fromkeys(arr1 + arr2))
+    return [x ** 3 for x in list(dict.fromkeys(arr1 + arr2))]
 
 
 print(zad_6([1, 2, 3, 4], [3, 4, 5, 6]))
