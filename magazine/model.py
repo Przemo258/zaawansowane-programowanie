200b7c0bf32b25c0e74b51fd835ff10c263bb994
Przemek <p.mordzewski@gmail.com>
2022-12-13 03:54:34 +0100

    projekt zaliczeniowy

---
+++ magazine/model.py
@@ -0,0 +1,52 @@
+import torch
+import cv2
+import time
+
+
+def get_model():
+    # wybieramy gdzie ma pobrać model i wagi :X
+    torch.hub.set_dir('magazine/')
+    # wczytanie modelu bez wyświetlania dodatkowych informacji
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s', verbose=False)
+    # jak jest możliwość wykonać na gpu ale niepotrzebne
+    # bo pytorch robi to automatycznie ale chce mieć 5
+    if torch.cuda.is_available():
+        model = model.cuda()
+    # tylko osoby
+    model.classes = [0]
+    return model
+
+
+def analize_image(image, model, save_img: bool = False):
+    # wczytanie zdjęcia
+    img = cv2.imread(image, cv2.IMREAD_COLOR)
+    # konwersja z BGR na RGB
+    rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    # rozpoczęcie mierzenia czasu
+    start = time.time()
+
+    # wrzucenie zdjęcia do modelu
+    model_results = model(rgb_img, size=640)
+
+    # zakończenie mierzenia czasu
+    end = time.time()
+
+    elapsed_time = end - start
+
+    # opcjonalne zapisanie zdjęcia do folderu (do podglądu)
+    if save_img:
+        model_results.save(save_dir='results/run')
+
+    # przekonwertowanie wyników na pandasowy dataframe
+    df = model_results.pandas().xyxy[0]
+    # jako że model wykrywa tylko ludzi liczba wierszy = liczba ludzi
+    count = df.shape[0]
+    # przekonwertowanie wyników na json
+    results_json = df.to_json()
+
+    results = {'total': count,
+               'results': results_json,
+               'elapsed_time': elapsed_time,
+               'description': str(model_results)}
+    return results
---
+++ main.py
@@ -0,0 +1,73 @@
+import uvicorn
+import os
+from fastapi import FastAPI, UploadFile, HTTPException, BackgroundTasks
+from fastapi.responses import FileResponse
+from magazine.model import get_model, analize_image
+
+app = FastAPI()
+model = get_model()
+
+
+@app.post('/')
+async def root(file: UploadFile):
+    # sprawdzamy czy to zdjęcie .jpg
+    if file.content_type != "image/jpeg":
+        raise HTTPException(400, detail="Invalid file type")
+    return analize_image(file.file, model)
+
+
+@app.post('/total')
+async def total(file: UploadFile):
+    # sprawdzamy czy to zdjęcie .jpg
+    if file.content_type != "image/jpeg":
+        raise HTTPException(400, detail="Invalid file type")
+    count = analize_image(file.file, model)['total']
+    return {'total': count}
+
+
+@app.post('/time')
+async def time(file: UploadFile):
+    # sprawdzamy czy to zdjęcie .jpg
+    if file.content_type != "image/jpeg":
+        raise HTTPException(400, detail="Invalid file type")
+    result = analize_image(file.file, model)
+    elapsed = result['elapsed_time']
+    description = result['description']
+    return {'elapsed_time': elapsed, 'description': description}
+
+
+@app.post('/boxes')
+async def time(file: UploadFile):
+    # sprawdzamy czy to zdjęcie .jpg
+    if file.content_type != "image/jpeg":
+        raise HTTPException(400, detail="Invalid file type")
+    data = analize_image(file.file, model)['results']
+    return data
+
+
+def cleanup_files():
+    # wchodzimy do folderu i usuwamy zdjęcie i folder w którym sie znajduje
+    os.chdir('results/run')
+    os.remove('image0.jpg')
+    os.chdir('../')
+    os.rmdir('run')
+    os.chdir('../')
+
+
+@app.post('/img')
+async def img(file: UploadFile, background_tasks: BackgroundTasks):
+    # sprawdzamy czy to zdjęcie .jpg
+    if file.content_type != "image/jpeg":
+        raise HTTPException(400, detail="Invalid file type")
+
+    # po wyświetleniu zdjęcia zostanie ono usunięte
+    background_tasks.add_task(cleanup_files)
+    _ = analize_image(file.file, model, save_img=True)
+
+    # zwracamy przeanalizowane zdjęcie
+    response = FileResponse('results/run/image0.jpg')
+    return response
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
